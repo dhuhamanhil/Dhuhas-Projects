88eb55cbe511465bc779625ab2f32564bb42db4b
dhuhamanhil <89488951+dhuhamanhil@users.noreply.github.com>
2021-08-24 17:54:49 -0400

    Lab 01 CMSC 255

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/***********************************************
+*Hello Java
+************************************************
+* Dhuha Manhil
+* 8/23/2021
+* CMSC 255 004
+************************************************/
+
+public class Hello {
+	public static void main(String[] args) {
+
+	    System.out.println("<Dhuha Manhil");
+	    System.out.println("<Hello Java>");
+	    System.out.println("<Lab01>");
+	    System.out.println("<August 23rd 2021>");
+	    System.out.println("<CMSC 255 004>");
+	    System.out.println();
+	}
+}
